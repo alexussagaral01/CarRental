14a7f30cfbb4e4ec757c44bfc74a5c34400186cc
Alexus Sundae Sagaral <alexussagaral3@gmail.com>
2025-04-15 21:21:47 +0700

    UPDATED AS OF 04/15/2025

--- transaction_details.php
+++ transaction_details.php
@@ -47,6 +47,17 @@
                     <li><a href="details.php" class="text-gray-600 hover:text-blue-600 font-medium transition-colors">About Us</a></li>
                 </ul>
             </nav>
+            
+            <div class="relative">
+                <div class="flex items-center bg-gray-100 rounded-full px-4 py-2 focus-within:ring-2 focus-within:ring-blue-500">
+                    <input type="text" placeholder="Search vehicles..." class="bg-transparent outline-none text-sm w-40 md:w-48">
+                    <button class="ml-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 text-gray-500">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
         </div>
     </header>
 
